136322a672525bb940fc5710397c2635f6790af8
Moises Fuentes <mfu-ent-es@csub.edu>
2024-03-19 20:30:22 -0700

    changed to polling

--- Frontend/src/components/Socket.jsx
+++ Frontend/src/components/Socket.jsx
@@ -6,8 +6,7 @@ import io from "socket.io-client";
 // });
 const socket = io("http://64.23.164.87", {
   path: "/api/socket.io",
-  transports: ["websocket"],
-  secure: true,
+  transports: ["polling"],
 });
 
 export default socket;
