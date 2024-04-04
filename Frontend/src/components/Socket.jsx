c8dd1fe494c2278cb4a15d705aa1e747ddf0e7a2
Moises Fuentes <mfu-ent-es@csub.edu>
2024-04-04 08:00:53 -0700

    error on line 9 for console log, removed console log

--- Frontend/src/components/Socket.jsx
+++ Frontend/src/components/Socket.jsx
@@ -4,11 +4,9 @@ import.meta.env.VITE_ENDPOINT;
 // const socket = io.connect(`${import.meta.env.VITE_ENDPOINT}{
 //     reconnection: false
 // });
-console.log(import.meta.env);
 if (import.meta.env.VITE_ENDPOINT === "https://64.23.164.87/api") {
   var pathValue = "/api/socket.io";
 }
-console.log(pathValue);
 const socket = io.connect(`${import.meta.env.VITE_SOCKET}`, {
   path: pathValue || "",
   transports: ["polling"],
