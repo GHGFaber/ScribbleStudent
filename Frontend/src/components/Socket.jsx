6a353651bb9dcd8fd854c93d73b32e0653915299
GHGFaber <moises.fuentes904@gmail.com>
2024-04-23 21:13:05 -0700

    fixing socket endpoints

--- Frontend/src/components/Socket.jsx
+++ Frontend/src/components/Socket.jsx
@@ -4,10 +4,14 @@ import.meta.env.VITE_ENDPOINT;
 // const socket = io.connect(`${import.meta.env.VITE_ENDPOINT}{
 //     reconnection: false
 // });
-var pathValue = null;
-if (import.meta.env.VITE_ENDPOINT === "https://scribblestudent/api") {
+let pathValue = "";
+let VITE_ENDPOINT = import.meta.env.VITE_ENDPOINT;
+console.log(VITE_ENDPOINT);
+if (VITE_ENDPOINT === "https://scribblestudent.com/api") {
   pathValue = "/api/socket.io";
 }
+
+console.log("FOUND PATH", pathValue);
 const socket = io.connect(`${import.meta.env.VITE_SOCKET}`, {
   path: pathValue || "",
   transports: ["polling"],
